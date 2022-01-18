d30a3e28bf0c46153f44839aac742f23ef3be711
sonangrai <shonahangrae@gmail.com>
2022-01-18 16:44:41 +0545

    auth model added

---
+++ src/model/Auth.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+/**
+ * A model for the user authentication
+ */
+
+const AuthSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Auth", AuthSchema);
