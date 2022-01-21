09a9d3a4e15345be43ad85f75ada842216bc54bb
sonangrai <shonahangrae@gmail.com>
2022-01-21 12:30:30 +0545

    mail paused

--- src/controller/Auth.js
+++ src/controller/Auth.js
@@ -2,6 +2,8 @@ import Auth from "../model/Auth.model";
 import { validationResult } from "express-validator";
 import ResponseObj from "./Response";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import nodemailer from "nodemailer";
 
 /**
  * This task is for registering a new user
@@ -47,14 +49,50 @@ export const RegisterTask = async (req, res) => {
   newUser.password = await bcrypt.hash(req.body.password, salt);
 
   /**
+   * Generating token for the activation link
+   */
+  let token = jwt.sign(
+    {
+      exp: Math.floor(Date.now() / 1000) + 60 * 60,
+      data: "foobar",
+    },
+    process.env.mySecret
+  );
+
+  /**
    * Saving to database
    */
   try {
     await newUser.save();
-    let resData = new ResponseObj(200, newUser, "User saved");
+    let resData = new ResponseObj(200, newUser, "Confirm link is sent to mail");
+    //SendActivationMail(newUser._id, token, newUser.email);
     return res.send(resData);
   } catch (error) {
     let resData = new ResponseObj(400, error, "User save failed");
     return res.send(resData);
   }
+};
+
+/**
+ * Create activate token and save to mongodb
+ */
+export const SendActivationMail = async (id, token, email) => {
+  console.log(email, token, id);
+
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    auth: {
+      user: "shonahangrae@gmail.com",
+      pass: process.env.myPass,
+    },
+  });
+
+  let info = await transporter.sendMail({
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to: email, // list of receivers
+    subject: "Password Reset Request", // Subject line
+    text: "Forgot Your Password", // plain text body
+    html: `<p>CLick here to change your password <a href="/api/resetpassword/${id}/${token}">Reset password</a> </p>`, // html body
+  });
+  console.log("Message sent: %s", info.messageId);
 };
