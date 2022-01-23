0c22ebe8bdd36ac93baf037891547a25a2415064
sonangrai <shonahangrae@gmail.com>
2022-01-23 11:14:34 +0545

    frontend redux and fonts init

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import { Provider } from "react-redux";
+import { ThemeProvider } from "styled-components";
+import Global from "../styled/Global.styled";
+import { theme } from "../styled/Theme";
+import store from "../redux/store";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    // The theme Provide
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        {/* The Globals */}
+        <Global />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
