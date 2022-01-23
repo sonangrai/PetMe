70e8041cc38624f59e36982b251bfea0309a7986
sonangrai <shonahangrae@gmail.com>
2022-01-23 16:10:35 +0545

    app -> server

--- src/index.js
+++ src/index.js
@@ -45,9 +45,9 @@ connectDb();
  */
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("frontend/build"));
+  server.use(express.static("frontend/build"));
 
-  app.get("*", (req, res) => {
+  server.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 }
