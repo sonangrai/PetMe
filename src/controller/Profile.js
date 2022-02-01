529ae2e1c659ba2302ec40e895b6954332d68168
sonangrai <shonahangrae@gmail.com>
2022-02-01 12:06:41 +0545

    edit profile

--- src/controller/Profile.js
+++ src/controller/Profile.js
@@ -1,4 +1,5 @@
 import Profile from "../model/Profile.model";
+import Auth from "../model/Auth.model";
 import { validationResult } from "express-validator";
 import ResponseObj from "./Response";
 
@@ -37,6 +38,63 @@ export const CreateProfileTask = async (req, res) => {
     res.send(respObject);
   } catch (error) {
     let resData = new ResponseObj(400, error, "Profile save failed");
+    return res.send(resData);
+  }
+};
+
+/**
+ * Edit a profile
+ */
+export const EditProfileTask = async (req, res) => {
+  let { firstname, lastname, avatar, address, contact } = req.body;
+
+  //Checking the validetion error
+  let errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    let respObject = new ResponseObj(
+      400,
+      errors.errors,
+      "Validation error occured"
+    );
+    return res.status(400).send(respObject);
+  }
+
+  let profile = new Profile({
+    firstname,
+    lastname,
+    avatar,
+    address,
+    contact,
+  });
+
+  //New pbject for updated fields
+  let newProfile = {};
+  if (firstname) newProfile.firstname = firstname;
+  if (lastname) newProfile.lastname = lastname;
+  if (avatar) newProfile.avatar = avatar;
+  if (address) newProfile.address = address;
+  if (contact) newProfile.contact = contact;
+
+  try {
+    let findUser = await Auth.findById(req.user.id);
+    if (!findUser) {
+      let respObject = new ResponseObj(400, newprofile, "User not found");
+      res.send(respObject);
+    } else {
+      profile = await Profile.findOneAndUpdate(
+        { _id: req.user.id },
+        { $set: newProfile },
+        { new: true }
+      );
+      let respObject = new ResponseObj(
+        200,
+        newProfile,
+        "Profile Update Success"
+      );
+      return res.status(200).send(respObject);
+    }
+  } catch (error) {
+    let resData = new ResponseObj(400, error, "Profile Update Error");
     return res.send(resData);
   }
 };
--- src/routes/profileRoutes.js
+++ src/routes/profileRoutes.js
@@ -1,6 +1,10 @@
 import { Router } from "express";
 import { check } from "express-validator";
-import { CreateProfileTask, GetProfile } from "../controller/Profile";
+import {
+  CreateProfileTask,
+  EditProfileTask,
+  GetProfile,
+} from "../controller/Profile";
 import auth from "../middleware/auth";
 
 let router = Router();
@@ -30,6 +34,33 @@ router.post(
   ],
   auth,
   CreateProfileTask
+);
+
+/**
+ * Create profile route
+ */
+router.put(
+  "/",
+  [
+    check("firstname")
+      .notEmpty()
+      .withMessage("This is a required field")
+      .matches(/^[A-Za-z\s]+$/)
+      .withMessage("This must be a string"),
+    check("lastname")
+      .notEmpty()
+      .withMessage("This is a required field")
+      .matches(/^[A-Za-z\s]+$/)
+      .withMessage("This must be a string"),
+    check("address").notEmpty().withMessage("This is a required field"),
+    check("contact")
+      .notEmpty()
+      .withMessage("This is a required field")
+      .matches(/^\d{10}$/)
+      .withMessage("This must be a valid number"),
+  ],
+  auth,
+  EditProfileTask
 );
 
 /**
