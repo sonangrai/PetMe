23e083b66eb64f8192576a306db4b2f19c5adaa5
sonangrai <shonahangrae@gmail.com>
2022-02-04 15:54:16 +0545

    updated mongoose and verify model

--- src/model/Verify.model.ts
+++ src/model/Verify.model.ts
@@ -3,8 +3,12 @@ import mongoose from "mongoose";
 /**
  * This model will hold the secret pin codes for sign up and reset pasword
  */
+export interface IVerify extends mongoose.Document {
+  email: string;
+  secret?: string;
+}
 
-const VerifySchema = new mongoose.Schema9(
+const VerifySchema = new mongoose.Schema(
   {
     email: {
       type: String,
@@ -14,7 +18,7 @@ const VerifySchema = new mongoose.Schema9(
       type: String,
     },
   },
-  { timestamp: true }
+  { timestamps: true }
 );
 
-export default mongoose("Verify", VerifySchema);
+export default mongoose.model<IVerify>("Verify", VerifySchema);
