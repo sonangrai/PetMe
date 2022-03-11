66cd6108dfd5b86d5739b3575e6a8af44817adb5
sonangrai <shonahangrae@gmail.com>
2022-03-11 16:30:50 +0545

    paginate in response class

--- src/controller/Response.ts
+++ src/controller/Response.ts
@@ -7,11 +7,13 @@
 class ResponseObj {
   status: number;
   data: object;
+  meta: object;
   msg: string;
 
-  constructor(status: number, data: object, msg: string) {
+  constructor(status: number, data: object, meta: object, msg: string) {
     this.status = status;
     this.data = data;
+    this.meta = meta;
     this.msg = msg;
   }
 }
