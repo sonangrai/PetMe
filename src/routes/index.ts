e3ceafba4c55de84bdef9c3f8fe359e02d62b4f8
sonangrai <shonahangrae@gmail.com>
2022-05-02 17:20:13 +0545

    removed older feeds endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,6 @@ import { Router } from "express";
 import authRoutes from "./authRoutes";
 import profileRoutes from "./profileRoutes";
 import bucketRoutes from "./bucketRoutes";
-import feedRoutes from "./feedRoutes";
 
 const router = Router();
 
@@ -20,10 +19,5 @@ router.use("/profile", profileRoutes);
  * Bucket routes
  */
 router.use("/bucket", bucketRoutes);
-
-/**
- * Feeds routes
- */
-router.use("/feed", feedRoutes);
 
 export default router;
