146399aaa0678afa60bfa9a167f2cc36230459fb
sonangrai <shonahangrae@gmail.com>
2022-05-09 17:47:59 +0545

    post feeds with media

--- src/v1/controller/Feed.ts
+++ src/v1/controller/Feed.ts
@@ -1,7 +1,16 @@
 import { Request, Response } from "express";
-import Feed, { Ifeed } from "../model/Feed.modal";
+import Feed from "../model/Feed.modal";
 import ResponseObj from "./Response";
 import respPagination from "./respPagination";
+import cloudinary from "cloudinary";
+require("dotenv").config();
+
+//Cloudinary config
+cloudinary.v2.config({
+  cloud_name: process.env.cloudName,
+  api_key: process.env.apiKey,
+  api_secret: process.env.apiSecret,
+});
 
 /**
  * Get all feeds
@@ -18,4 +27,71 @@ export const getFeedTask = async (req: Request, res: Response) => {
     }
     return res.send(feeds);
   } catch (error) {}
+};
+
+/**
+ * Post New Feed
+ * @param req
+ * @param res
+ */
+export const postFeedTask = async (req: Request, res: Response) => {
+  try {
+    const { description, userId, type } = req.body;
+
+    let newFeed = new Feed({
+      description,
+      userId,
+      type,
+      media: [],
+    });
+
+    let medias = req.files.media;
+
+    //Uploading all the media images to cloudinary
+    let multiplePicturePromise = medias.map(
+      (element) =>
+        new Promise((resolve, reject) => {
+          //Uploading to Cloudinary
+          cloudinary.v2.uploader.upload(
+            element.path,
+            {
+              folder: "petgram/post",
+              use_filename: true,
+            },
+            function (error, result) {
+              if (error) {
+                reject(error);
+              } else {
+                let media = {
+                  public_id: result.public_id,
+                  url: result.url,
+                  secure_url: result.secure_url,
+                  width: result.width,
+                  height: result.height,
+                };
+                resolve(media);
+              }
+            }
+          );
+        })
+    );
+    let imageResponses = await Promise.all(multiplePicturePromise);
+
+    newFeed.media = imageResponses;
+
+    //Saving the feeds info
+    await newFeed.save();
+    let respObject = new ResponseObj(
+      200,
+      newFeed,
+      {},
+      "Post added successfully"
+    );
+    return res.status(200).send(respObject);
+  } catch (error) {
+    let errorObject: object = {};
+    if (error instanceof Error) errorObject = error;
+    let resData = new ResponseObj(400, errorObject, {}, "Post save failed");
+    return res.send(resData);
+  }
 };
--- src/v1/routes/feedRoutes.ts
+++ src/v1/routes/feedRoutes.ts
@@ -1,12 +1,20 @@
 import { Router } from "express";
-import { getFeedTask } from "../controller/Feed";
+import { getFeedTask, postFeedTask } from "../controller/Feed";
 import auth from "../middleware/auth";
+import multipart from "connect-multiparty";
+
+let multipartMiddleware = multipart();
 
 const router = Router();
 
 /**
- *
+ *get feed
  */
 router.get("/", auth, getFeedTask);
+
+/**
+ *Post new feed
+ */
+router.post("/", multipartMiddleware, postFeedTask);
 
 export default router;
