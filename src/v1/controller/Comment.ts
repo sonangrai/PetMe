1702c6b99a9d505a516f4cbf7749a83d368b57a8
sonangrai <shonahangrae@gmail.com>
2022-05-18 17:35:08 +0545

    add comment done

--- src/v1/controller/Comment.ts
+++ src/v1/controller/Comment.ts
@@ -1,5 +1,6 @@
 import Comment from "../model/Comment.modal";
 import { Response, Request } from "express";
+import ResponseObj from "./Response";
 
 /**
  * Post New Comment
@@ -8,7 +9,26 @@ import { Response, Request } from "express";
  */
 export const postCommentTask = async (req: Request, res: Response) => {
   try {
-    const { comment, replyTo } = req.body;
-    res.send(comment);
-  } catch (error) {}
+    const { commentText, feedId, commentorId, replyTo } = req.body;
+    let newComment = new Comment({
+      feedId,
+      commentorId,
+      commentText,
+      replyTo,
+    });
+    await newComment.save();
+
+    let respObject = new ResponseObj(
+      200,
+      newComment,
+      {},
+      "Comment added successfully"
+    );
+    return res.status(200).send(respObject);
+  } catch (error) {
+    let errorObject: object = {};
+    if (error instanceof Error) errorObject = error;
+    let resData = new ResponseObj(400, errorObject, {}, "Post save failed");
+    return res.send(resData);
+  }
 };
