c7a10530b851b924d916e42428996a18f8f32ecc
sonangrai <shonahangrae@gmail.com>
2022-05-19 11:50:14 +0545

    added controller for the comment delete

--- src/v1/controller/Comment.ts
+++ src/v1/controller/Comment.ts
@@ -45,3 +45,29 @@ export const postCommentTask = async (req: Request, res: Response) => {
     return res.send(resData);
   }
 };
+
+export const deleteCommentTask = async (req: Request, res: Response) => {
+  try {
+    //Updating the comment count in the post
+
+    await Feed.findOneAndUpdate(
+      { _id: req.params.fId },
+      { $dec: { commentCount: 1 } }
+    );
+
+    await Comment.deleteOne({ _id: req.params.cId }); //Delete the commment
+
+    let respObject = new ResponseObj(200, {}, {}, "Comment Deleted");
+    return res.status(200).send(respObject);
+  } catch (error) {
+    let errorObject: object = {};
+    if (error instanceof Error) errorObject = error;
+    let resData = new ResponseObj(
+      400,
+      errorObject,
+      {},
+      "Comment delete failed"
+    );
+    return res.send(resData);
+  }
+};
