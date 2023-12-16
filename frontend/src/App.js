a54360833f16d1d3ecafcf6ba2138ccb9f633574
sonangrai <shonahangrae@gmail.com>
2023-12-16 19:35:37 +0545

    removed fe

--- frontend/src/App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Card from "@mui/material/Card";
-
-const App = () => {
-  return <Card variant="outlined">Admin Dashboard will be here</Card>;
-};
-
-export default App;
